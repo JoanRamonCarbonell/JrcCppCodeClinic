92d075414b97f72f4ad6a965f7629025b4dec483
jr <j.r.carbonell@hotmail.com>
2025-06-04 15:21:56 +0200

    EightQueens phase1

--- include/EightQueens/EightQueens.h
+++ include/EightQueens/EightQueens.h
@@ -1,19 +1,34 @@
 #pragma once
 
 #include "IEightQueens.h"
-#include <vector>
+#include <array>
 #include <string>
-
+#include <map>
+#include <utility>
 namespace eight_queens
 {
+constexpr auto POS_NUMBER{8};
+constexpr auto QUEENS_NUM{8};
+constexpr auto QUEEN{'Q'};
+constexpr auto COLUMN_LIMIT{" | "};
+const std::string BOARD_LIMIT{" +===============================+"};
+const std::string BOARD_INTERLINE{" |-------------------------------|"};
+
+
 
 class EightQueens : public IEightQueens
 {
 public:
     EightQueens() { run(); }
     void run() override;
+    void paint_chess_board();
+    bool is_queen_in_row(int& row);
+    bool is_queen_in_col(int& col);
+    bool is_queen_in_diagonal(int& row, int& col);
 
 private:
-
+    std::array<std::array<char, POS_NUMBER>, POS_NUMBER> m_chess_board;
+    int m_located_queens{0};
+    std::map<int, std::pair<int, int>> m_queens_position;
 };
 } // eight_queens
--- src/EightQueens/EightQueens.cpp
+++ src/EightQueens/EightQueens.cpp
@@ -9,6 +9,82 @@ void EightQueens::run() {
 
     cout << "Running : Eight Queens" << endl;
 
+    // Locate 1st queen (hardcoded in v1)
+    auto locate_queen{[&](int i, int j){
+        m_chess_board[i][j] = QUEEN;
+        m_queens_position.insert({m_located_queens, {i, j}});
+        cout << "### Queen located at : (" << i << "," << j << ") ###" << endl;
+        m_located_queens++;
+    }};
+
+    locate_queen(3,5);
+    while (m_located_queens < QUEENS_NUM) {
+        // locate next queen
+        for (auto i=0; i<POS_NUMBER ; i++) {
+            if (!is_queen_in_row(i)) {
+                for (auto j=0; j<POS_NUMBER ; j++) {
+                    if (!is_queen_in_col(j)) {
+                        if (!is_queen_in_diagonal(i,j)) {
+                            locate_queen(i,j);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Paint chess board
+    paint_chess_board();
+}
+
+bool EightQueens::is_queen_in_row(int& row) {
+    for (const auto& [key, pair] : m_queens_position) {
+        if (row == pair.first) { 
+            return true;
+        }
+    }
+    return false;
+}
+
+bool EightQueens::is_queen_in_col(int& col) {
+    for (const auto& [key, pair] : m_queens_position) {
+        if (col == pair.second) { 
+            return true;
+        }
+    }
+    return false;
+}
+
+bool EightQueens::is_queen_in_diagonal(int& i, int& j) {
+    return false;
+}
+
+void EightQueens::paint_chess_board() {
+
+    std::string line;
+
+    cout << "*** Chess board: ***" << endl;
+    cout << BOARD_LIMIT << endl;
+    int interline_counter{0};
+
+    for (const auto& row : m_chess_board) {
+        line = "";
+        line += COLUMN_LIMIT;
+        for (const auto& col : row) {
+            line += (col == '\0' ? ' ' : col );
+            line += COLUMN_LIMIT;
+        }
+        cout << line << endl;
+        interline_counter++;
+        if (interline_counter < POS_NUMBER) { 
+            cout << BOARD_INTERLINE << endl;
+        }
+    }
+    cout << BOARD_LIMIT << endl;
+
     cout << "End : Eight Queens" << endl;
 }
+
+
 } // eight_queens
